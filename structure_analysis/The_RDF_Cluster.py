25ec67726992bedcb0c746a77ff1b6d7dec39a16
Tonggih Kang <uccatka@login04.thomas.ucl.ac.uk>
2022-06-22 17:45:27 +0100

    ed

--- structure_analysis/The_RDF_Cluster.py
+++ structure_analysis/The_RDF_Cluster.py
@@ -1,7 +1,7 @@
+#pip install plotly
 import plotly.express as px
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import os, sys
 from itertools import combinations
 
@@ -124,8 +124,6 @@ with open('output.csv', 'w') as f:
 #pip install plotly
 
 import plotly.express as px
-import pandas as pd
-
 dff = pd.read_csv('output.csv')
 fig = px.line(dff, x=dff.columns[0], y=dff.columns[1], title=None)
 fig.show()
